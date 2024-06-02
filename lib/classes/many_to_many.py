df141ad575b26317555376af71e1035cca59ac1e
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-06-02 23:26:55 +0300

    done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,97 +1,73 @@
 class Article:
    def __init__(self, author, magazine, title):
-       if not isinstance(author, Author):
-            raise ValueError("author must be of type Author")
-       if not isinstance(magazine, Magazine):
-            raise ValueError("magazine must be of type Magazine") 
-       self.__author = author
-       self.__magazine = magazine
-       self.__title = title
+       if not isinstance(title, str) or len(title) < 5 or len(title) > 50:
+            raise ValueError("Unavailable article title")
+       self._author = author
+       self._magazine = magazine
+       self._title = title   
        author.articles().append(self)
        magazine.articles().append(self)
-        
-   @property
+
+   @property 
    def title(self):
-       return self.__title
+       return self._title
 
    @property
    def author(self):
-       return self.__author
+       return self._author
 
    @property
    def magazine(self):
-       return self.__magazine
-   @magazine.setter
-   def magazine(self, new_magazine):
-        if not isinstance(new_magazine, Magazine):
-            raise ValueError("magazine must be of type Magazine")
-        self.__magazine = new_magazine
-
-   @author.setter
-   def author(self, new_author):
-        if not isinstance(new_author, Author):
-            raise ValueError("author must be of type Author")
-        self.__author = new_author    
+       return self._magazine
 
 class Author:
     def __init__(self, name):
-        if not isinstance(name, str) or len(name) == 0 :
-            raise ValueError("Name must be a non-empty string")
-        self.__name = name
-        self.__articles = []
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name is a string")
+        self._name = name
+        self._articles = []
 
     @property
     def name(self):
-        return self.__name
+        return self._name
 
     def articles(self):
-        return self.__articles
+        return self._articles
 
     def magazines(self):
-        return list(set(article.magazine for article in self.__articles))
+        return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
         article = Article(self, magazine, title)
-        self.__articles.append(article)
+        self._articles.append(article)
         return article
 
     def topic_areas(self):
-        if not self.__articles:
+        if not self._articles:
             return None
-        return list(set(article.magazine.category for article in self.__articles))
+        return list(set(article.magazine.category for article in self._articles))
     
 class Magazine:
-   __all_magazines = []
-
    def __init__(self, name, category):
-        if not isinstance(name, str) or len(name) < 2 or len(name) > 16:
-            raise ValueError("Magazine name must be a string between 2 and 16 characters")
-        if not isinstance(category, str) or len(category) == 0:
-            raise ValueError("Category must be a non-empty string")
-        self.__name = name
-        self.__category = category
-        self.__articles = []
+       assert isinstance(name,str) and 2 <= len(name) <= 16
+       assert isinstance(category,str) and len(category) > 0 
+       self.name = name
+       self.category = category
+       self.articles_published = []
         
-
-   @property
-   def name(self):
-        return self.__name
-   @property
-   def category(self):
-        return self.__category
-
    def articles(self):
-        return self.__articles
+       return self.articles_published
 
    def contributors(self):
-        authors = [article.author for article in self.__articles]
-        return list(set(authors))
+       authors = [article.author for article in self.articles_published]
+       return list(set(authors))
 
    def article_titles(self):
-        return [article.title for article in self.__articles]
+        return [article.title for article in self.articles_published]
 
    def contributing_authors(self):
-        authors = [article.author for article in self.__articles]
-        return [author for author in authors if authors.count(author) > 2]
-
-   
+       author_counts = {}
+       for article in self.articles_published:
+            author_name = article.author.name
+            author_counts[author_name] = author_counts.get(author_name, 0) + 1
+       return [author for author, count in author_counts.items() if count > 2]
